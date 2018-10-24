327797193bf2a5cf1eb4aafe9c8fd2cec540e281
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-10-24 21:15:22 +0200

    - Main. Rough design for root to have a button to reproduce radio station

--- RadioFmApp/Code/Controllers/RootViewController.swift
+++ RadioFmApp/Code/Controllers/RootViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class RootViewController: UIViewController {
 
+    // MARK: - Properties
+
+    /** Property that represents the button for the view */
+    @IBOutlet weak var button: UIButton!
+
+    // MARK: - Inherited functions from UIView controller
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.topViewController?.navigationItem.title = "RadiOS FM"
+        self.button.setTitle("PLAY", for: .normal)
+    }
+
+    // MARK: - IBAction function implementation
+
+    /**
+     Function that performs an action when the menu button is clicked
+     - parameter sender: The identifier of the sender of the action
+     */
+    @IBAction func didPress(_ sender: UIButton) {
+        NSLog("didPress(_:)")
     }
 }
