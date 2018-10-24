046cbbda97c76b57d86f40277ee0e88054984d14
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-10-24 20:59:28 +0200

    - AudioUtils. Implemented util class

--- RadioFmApp/Code/Configuration.swift
+++ RadioFmApp/Code/Configuration.swift
@@ -35,3 +35,40 @@ struct Color {
     static let kEB004E = UIColor(red: 235/255, green: 0/255, blue: 78/255, alpha: 1) // protectonaut red
     static let kE7B639 = UIColor(red: 231/255, green: 182/255, blue: 57/255, alpha: 1) // protectonaut accent yellow
 }
+
+// Constants for string tags
+struct Tag {
+    static let At = "@"
+    static let Blank = " "
+    static let BlankSlash = " |"
+    static let Br = "<br>"
+    static let Button = "button"
+    static let Config = "tests_config"
+    static let Comma = ","
+    static let Dialog = "dialog"
+    static let Dot = "."
+    static let Empty = ""
+    static let ErrorFormat = "[Bundle] Error! An error ocurred getting the data. Hint: "
+    static let Filter = "filter"
+    static let Fix = "fix"
+    static let Flow = "flow"
+    static let HundredK = "100000"
+    static let Issues = "issue_tree"
+    static let Item = "item"
+    static let Minus = "-"
+    static let NewLine = "\n"
+    static let Plus = "+"
+    static let Prereqs = "prereqs"
+    static let Results = "results_evaluation"
+    static let Slash = "|"
+    static let SlashDouble = "||"
+    static let Slash2 = "/"
+    static let Solutions = "solutions"
+    static let Test = "test"
+    static let TestGroup = "test_group"
+    static let TestOk = "test_ok"
+    static let Unknown = "unknown"
+    static let Value = "value"
+    static let WhiteList = "white_list_app"
+    static let XML = "xml"
+}
--- RadioFmApp/Code/Utils/AudioUtils.swift
+++ RadioFmApp/Code/Utils/AudioUtils.swift
@@ -28,6 +28,193 @@ protocol AudioDelegate: class {
     func util(_ util: AudioUtils, didFinish success: Bool)
 }
 
-class AudioUtils: NSObject {
+class AudioUtils: NSObject, AVAudioPlayerDelegate, AVSpeechSynthesizerDelegate {
 
+    // MARK: - Properties
+
+    /** Property that represents the delegate of the utils */
+    weak var delegate: AudioDelegate?
+    /** Property that represents the audio player */
+    private var player: AVAudioPlayer?
+    /** Property that represents the audio synthesizer */
+    private var synthesizer: AVSpeechSynthesizer?
+    /** Property that represents the current volume */
+    private var outputVolume: Float = 0.0
+    /** Property that represents the minimum volume threshold */
+    private var minVolume: Float = 0.0
+    /** Property that represents the old volume of the audio player */
+    private var oldVolume: Float = 0.0
+    /** Property that represents the flag whether the volume is too low or not */
+    private var isTooLow: Bool = false
+    /** Property that represents the flag whether the device is muted or not */
+    private var isMuted: Bool = false
+
+    // MARK: - Singleton
+
+    static let shared = AudioUtils()
+    private override init() {
+        // This prevents others from using the default '()' initializer for this class.
+    }
+
+    // MARK: - Inherited function from AVAudioPlayer delegate
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.deactivateSession()
+        if Verbose.Active { NSLog(flag ? "[AVAudioPlayer] Log: Player finished playing..." : "[AVAudioPlayer] Warning: Something happened while playing...") }
+        if self.delegate != nil { self.delegate?.util(self, didFinish: flag) }
+    }
+
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if player.isPlaying { player.stop() }
+        NSLog("[AVAudioPlayer] Error! An error occurred playing! Error 502 - \(error?.localizedDescription ?? Tag.Unknown)")
+        if self.delegate != nil { self.delegate?.util(self, didFinish: false) }
+    }
+
+    // MARK: - Inherited function from Speech synthesizer delegate
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) {
+        if Verbose.Active { NSLog("[AVSpeechSynthesizer] Log: didStart...") }
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didPause utterance: AVSpeechUtterance) {
+        if Verbose.Active { NSLog("[AVSpeechSynthesizer] Log: didPause...") }
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) {
+        if Verbose.Active { NSLog("[AVSpeechSynthesizer] Log: didCancel...") }
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didContinue utterance: AVSpeechUtterance) {
+        if Verbose.Active { NSLog("[AVSpeechSynthesizer] Log: didContinue...") }
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        if Verbose.Active { NSLog("[AVSpeechSynthesizer] Log: didFinish...") }
+        if self.delegate != nil { self.delegate?.util(self, didFinish: true) }
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+        if Verbose.Active { NSLog("[AVSpeechSynthesizer] Log: willSpeakRangeOfSpeechString...") }
+    }
+
+    // MARK: - Functions
+
+    /**
+     Function that configures the audio util and device
+     */
+    func configure() {
+        do { // try to get all configuration ready
+            try activateSession()
+        } catch { // handle error
+            if self.delegate != nil { self.delegate?.util(self, canSpeak: false) }
+        }
+        self.volumeChanged()
+        self.muteSwitchWillUpdate()
+    }
+
+    /**
+     Function that deactivates the audio util
+     */
+    func deactivate() {
+        self.deactivateSession()
+        self.delegate = nil
+    }
+
+    /**
+     Function that speaks the message received
+     - parameter message: The string-value for the message
+     */
+    func speak(_ message: String) {
+        if Verbose.Active { NSLog("[AVSpeechSynthesizer] Log: Speak \(message)...") }
+        let utterance = AVSpeechUtterance(string: message)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        self.synthesizer = AVSpeechSynthesizer()
+        self.synthesizer!.delegate = self
+        self.synthesizer!.speak(utterance)
+    }
+
+    // MARK: - Private functions
+
+    /**
+     Function that activates the audio session
+     - throws: Set active error
+     */
+    private func activateSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        do { // Try to setup the audio sesion and read the volume
+            try session.setActive(true)
+            self.outputVolume = session.outputVolume
+        } catch { NSLog("[AVAudioSession] Error! An error ocurred activating the audioSession. Error 500 - \(error.localizedDescription)") }
+    }
+
+    /**
+     Function that deactivates the audio session
+     */
+    private func deactivateSession() {
+        NotificationCenter.default.removeObserver(self, name: AVAudioSession.routeChangeNotification, object: nil)
+        if let synthesizer = self.synthesizer { // stop synthesizer
+            if synthesizer.isSpeaking { synthesizer.stopSpeaking(at: .immediate) }
+            synthesizer.delegate = nil
+            self.synthesizer = nil
+        }
+        if let player = self.player { // stop player
+            if player.isPlaying { player.stop() }
+            player.delegate = nil
+            self.player = nil
+        }
+        do { // Try to deactive the audio sesion
+            try AVAudioSession.sharedInstance().setActive(false)
+        } catch { NSLog("[AVAudioSession] Error! An error ocurred deactivating the audioSession. Error 501 - \(error.localizedDescription)") }
+    }
+
+    /**
+     Function that checks if the volume is silenced
+     */
+    private func muteSwitchWillUpdate() {
+        Mute.shared.isPaused = false
+        Mute.shared.checkInterval = 1.0
+        Mute.shared.alwaysNotify = true
+        Mute.shared.notify = { silent in
+            Mute.shared.isPaused = true
+            if silent { // is silent
+                NotificationCenter.default.post(name: .alertNotification, object:["SPEAKER_MUTED_TITLE", "SPEAKER_MUTED_DESC", "WTG_BUTTON_OK"])
+            } else { if self.delegate != nil { self.delegate?.util(self, canSpeak: true) } }
+        }
+    }
+
+    /**
+     Function that play the audio
+     */
+    private func play() {
+        if let path = Bundle.main.path(forResource: "wtg-music", ofType: "wav") {
+            do { // play the audio file
+                self.player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                self.player!.delegate = self
+                self.player!.play()
+            } catch { NSLog("[AVAudioPlayer] Error! An error occurred trying to init the player. Error 502 - \(error.localizedDescription)") }
+        }
+    }
+
+    /**
+     Function that checks if the volume changed
+     */
+    private func volumeChanged() {
+        let isTooLow = self.outputVolume < self.minVolume
+        if isTooLow { self.volumeWillChange() }
+    }
+
+    /**
+     Function that configures the volume
+     */
+    private func volumeWillChange() {
+        let volumeView = MPVolumeView()
+        for view in volumeView.subviews {
+            if view is UISlider {
+                if let slider = view as? UISlider {
+                    self.oldVolume = slider.value == 0.0 ? 0.2 : slider.value
+                    slider.value = 0.6
+                }
+            }
+        }
+    }
 }
