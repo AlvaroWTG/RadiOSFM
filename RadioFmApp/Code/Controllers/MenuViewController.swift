12b431644efeb68d2af26a7736ab2f9a8eff1b52
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-10-28 12:52:46 +0100

    - Project. Added two main view controllers to project

---
+++ RadioFmApp/Code/Controllers/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  RadioFmApp
+//
+//  Created by Alvaro on 28/10/18.
+//  Copyright © 2018 Alvaro. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ RadioFmApp/Code/Controllers/TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewController.swift
+//  RadioFmApp
+//
+//  Created by Alvaro on 28/10/18.
+//  Copyright © 2018 Alvaro. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
