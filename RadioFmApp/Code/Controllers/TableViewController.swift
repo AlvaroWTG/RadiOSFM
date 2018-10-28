c3bf8d8a79c58cca4733ce6c0f501a2b91a3fba7
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-10-28 13:36:05 +0100

    - TableVc. Implemented inherited functions from table view data source and delegate

--- RadioFmApp/Code/Controllers/TableViewController.swift
+++ RadioFmApp/Code/Controllers/TableViewController.swift
@@ -39,5 +39,40 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.topViewController?.navigationItem.title = "RadiOS FM"
+        self.tableView.tableFooterView = UIView(frame: .zero)
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.button.setTitle("PLAY", for: .normal)
+    }
+
+    // MARK: - Inherited functions from UITableView data source
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainViewCell", for: indexPath) as! MainViewCell
+        self.tableView = tableView
+        cell.backgroundColor = .white
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    // MARK: - Inherited functions from UITableView delegate
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
     }
 }
