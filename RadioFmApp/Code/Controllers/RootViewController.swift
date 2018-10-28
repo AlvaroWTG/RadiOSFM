88b3965214667b63a1487568226ac9c541b98c03
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-10-28 12:25:35 +0100

    - RootVc. Implemented behaviour to configure&play a radio station

--- RadioFmApp/Code/Controllers/RootViewController.swift
+++ RadioFmApp/Code/Controllers/RootViewController.swift
@@ -37,6 +37,23 @@ class RootViewController: UIViewController, RadioDelegate {
      - parameter sender: The identifier of the sender of the action
      */
     @IBAction func didPress(_ sender: UIButton) {
-        NSLog("didPress(_:)")
+        let urls = ["http://195.10.10.222/cope/megastar.aac?GKID=d51d8e14d69011e88f2900163ea2c744", "http://195.55.74.203/rtpa/live/radio.mp3?GKID=280fad92d69a11e8b65b00163e914", "http://rne-hls.flumotion.com/playlist.m3u8", "http://94.75.227.133:1025/", "http://rac105.radiocat.net/", "http://playerservices.streamtheworld.com/api/livestream-redirect/CADENASERAAC_SC", " http://live.radiovoz.es/coruna/master.m3u8", "http://radios-ec.cdn.nedmedia.io/radios/ec-galaxia.m3u8"]
+        RadioUtils.shared.configure(urls[7])
+        RadioUtils.shared.delegate = self
+    }
+    
+    
+    // MARK: - IBAction function implementation
+
+    /**
+     Function that refreshes the artwork
+     - parameter url: The url or the artwork
+     */
+    private func refreshArtwork(_ url: URL) {
+        do { // download image
+            let data = try Data(contentsOf: url)
+            NSLog("[FRadioPlayer] Log: artwork changed @ \(url.absoluteString)")
+            DispatchQueue.main.async { self.imageView.image = UIImage(data: data) }
+        } catch { NSLog("Exception!") }
     }
 }
