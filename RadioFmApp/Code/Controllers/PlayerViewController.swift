40a7bbe05f474fa07595eb603f9ac2507ed1263a
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-11-13 16:34:16 +0100

    - PlayerVc. Design view controller

--- RadioFmApp/Code/Controllers/PlayerViewController.swift
+++ RadioFmApp/Code/Controllers/PlayerViewController.swift
@@ -12,6 +12,19 @@ class PlayerViewController: UIViewController {
 
     // MARK: - Properties
 
+    /** Property that represents the header for the advertising */
+    @IBOutlet weak var headerMarketing: UIView!
+    /** Property that represents the image view for the player */
+    @IBOutlet weak var imageView: UIImageView!
+    /** Property that represents the label for the player */
+    @IBOutlet weak var labelPlayer: UILabel!
+    /** Property that represents the image view for the favorite option */
+    @IBOutlet weak var imageFavorite: UIImageView!
+    /** Property that represents the view for the play button */
+    @IBOutlet weak var viewPlay: UIView!
+    /** Property that represents the icon for the play button */
+    @IBOutlet weak var iconPlay: UIImageView!
+
     // MARK: - Inherited functions from UIView controller
 
     override func viewDidLoad() {
