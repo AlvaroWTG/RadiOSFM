1a8bcad4bfccb45aeb7bef34bfb7479963861e45
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-11-13 16:35:32 +0100

    - PlayerVc. Setup view controller on viewDidLoad()

--- RadioFmApp/Code/Controllers/PlayerViewController.swift
+++ RadioFmApp/Code/Controllers/PlayerViewController.swift
@@ -34,5 +34,36 @@ class PlayerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        // Setup navigation bar for the view controller
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.topViewController?.navigationItem.title = self.station.name
+
+        // Setup view controller content
+        self.headerMarketing.backgroundColor = .lightGray
+        self.imageView.image = UIImage(named: self.station.iconName)
+        self.labelPlayer.text = "\(self.station.name)\n\(self.station.popularity)"
+        self.imageFavorite.image = UIImage(named: self.station.isFavorite ? "star_full" : "star_empty")
+        self.imageView = ColorUtils.shared.renderImage(self.imageView, color: .lightGray, userInteraction: true)
+        if !self.station.isFavorite { self.imageFavorite = ColorUtils.shared.renderImage(self.imageFavorite, color: Color.k1097FB, userInteraction: true) }
+
+        // Setup button play view
+        self.iconPlay.image = UIImage(named: self.isPlaying ? "pause" : "play")
+        self.viewPlay.layer.cornerRadius = self.viewPlay.frame.width / 2
+        self.viewPlay.clipsToBounds = true
+
+        // Swap Back button
+        NotificationCenter.default.post(name: .swapBackButton, object: nil)
+    }
+
+    // MARK: - Functions
+
+    /**
+     Function that removes the present view controller
+     - parameter sender: The button sender of the action
+     */
+    @objc private func didPressBack(_ sender: UIButton?) {
+        self.navigationController?.popViewController(animated: true)
+        NotificationCenter.default.post(name: .swapBackButton, object: nil)
     }
 }
