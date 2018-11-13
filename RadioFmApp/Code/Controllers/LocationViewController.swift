f786a39ebfa93e33320f9e38922ef5b7592f4232
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-11-13 15:53:35 +0100

    - project. Added player and location view controllers

---
+++ RadioFmApp/Code/Controllers/LocationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LocationViewController.swift
+//  RadioFmApp
+//
+//  Created by WebToGo on 11/13/18.
+//  Copyright © 2018 Alvaro. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - Inherited functions from UIView controller
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ RadioFmApp/Code/Controllers/PlayerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerViewController.swift
+//  RadioFmApp
+//
+//  Created by WebToGo on 11/13/18.
+//  Copyright © 2018 Alvaro. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - Inherited functions from UIView controller
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
