e985f7c4e99675b2a8244bde718bf227b28efd0d
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2018-11-19 12:34:04 +0100

    - LocationVc. Implemented inherited functions from location manager delegate

--- RadioFmApp/Code/Controllers/LocationViewController.swift
+++ RadioFmApp/Code/Controllers/LocationViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Crashlytics
+import CoreLocation
 
 class LocationViewCell: UITableViewCell {
 
@@ -15,6 +17,8 @@ class LocationViewCell: UITableViewCell {
     /** Property that represents the label for the cell */
     @IBOutlet weak var labelTitle: UILabel!
 }
+
+class LocationViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
 
     // MARK: - Properties
 
@@ -31,4 +35,62 @@ class LocationViewCell: UITableViewCell {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    // MARK: - Inherited functions from Core location manager
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let geocoder = CLGeocoder()
+            manager.stopUpdatingLocation()
+            geocoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+                if let placemark = placemarks?.last {
+                    NSLog("[CLGeocoder] Log: Placemark country code: \(placemark.isoCountryCode ?? "unknown")")
+                } else if error != nil {
+                    NSLog("Error! An error occurred with reverse geodecode location! Error 405 - \(error!.localizedDescription)")
+                    let userInfo = [NSLocalizedDescriptionKey : "CLLocationManager - Failed to greverse geocode location",
+                                    NSLocalizedFailureReasonErrorKey : "405 - \(error!.localizedDescription)"]
+                    Crashlytics.sharedInstance().recordError(NSError(domain: Api.ErrorDomain, code: -1001, userInfo: userInfo))
+                } else { // placemark not found
+                    NSLog("Error! An error occurred with reverse geodecode location! Error 406 - Last placemark not found")
+                    let userInfo = [NSLocalizedDescriptionKey : "CLLocationManager - Failed to get last placemark",
+                                    NSLocalizedFailureReasonErrorKey : "406 - Last placemark not found"]
+                    Crashlytics.sharedInstance().recordError(NSError(domain: Api.ErrorDomain, code: -1001, userInfo: userInfo))
+                }
+            })
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        manager.stopUpdatingLocation()
+        NSLog("Error! Location didFailWithError! Error 404 - \(error.localizedDescription)")
+        let userInfo = [NSLocalizedDescriptionKey : "CLLocationManager - Location didFailWithError",
+                        NSLocalizedFailureReasonErrorKey : "404 - \(error.localizedDescription)"]
+        Crashlytics.sharedInstance().recordError(NSError(domain: Api.ErrorDomain, code: -1001, userInfo: userInfo))
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+            case .notDetermined:
+                if Verbose.Active { NSLog("[CoreLocation] Log: Location access isn't determined...") }
+                manager.requestWhenInUseAuthorization()
+                break
+            case .authorizedWhenInUse:
+                if Verbose.Active { NSLog("[CoreLocation] Log: Location access is authorized when in use...") }
+                manager.startUpdatingLocation()
+                break
+            case .authorizedAlways:
+                if Verbose.Active { NSLog("[CoreLocation] Log: Location access is always authorized...") }
+                manager.startUpdatingLocation()
+                break
+            case .restricted:
+                if Verbose.Active { NSLog("[CoreLocation] Error! Location access is restricted!") }
+                self.performSelector(onMainThread: #selector(self.pushAlert), with: nil, waitUntilDone: false)
+                break
+            case .denied:
+                if Verbose.Active { NSLog("[CoreLocation] Error! Location access is denied!") }
+                self.performSelector(onMainThread: #selector(self.pushAlert), with: nil, waitUntilDone: false)
+                break
+        }
+    }
+
 }
